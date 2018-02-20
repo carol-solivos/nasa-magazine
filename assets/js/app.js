23677f65e4a09d78b2f3c9c159aa89f905106379
carol-solivos <carol.solivos@gmail.com>
2018-02-20 19:32:40 -0400

    conectado con foto del día automáticamente

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,31 @@
 
 //https://api.nasa.gov/api.html#EPIC
-var url = "https://api.nasa.gov/planetary/apod?date=2018-02-18&api_key=Tqbm56QokO6KA0u6wjbcnSVeiYwlsq9dNK46y7fE";
+//tomar fecha para actualizar imagen cada día
+toDayImage();
+function toDayImage() {
+  let d = new Date();
+  let arrMonth = ['01','02','03','04','05','06','07','08','09','10','11','12',];
+  let toDay = d.getDate();
+  let toMonth = d.getMonth();
+  let toYear = d.getFullYear();
 
-$.ajax({
-  url: url,
-  success: function(result){
-    const imgs = result.url;
-    const title = result.title;
-    const explanation = result.explanation;
-    console.log(result);
-    $('#container').html(`<img src="${imgs}" alt="">`);
-  }
-});
+  if (toDay < 10) {
+    toDay = '0' + toDay;
+  };
+
+  let url = `https://api.nasa.gov/planetary/apod?date=${toYear}-${arrMonth[toMonth]}-${toDay}&api_key=Tqbm56QokO6KA0u6wjbcnSVeiYwlsq9dNK46y7fE`;
+
+  $.ajax({
+    url: url,
+    success: function(result){
+      const imgs = result.url;
+      const title = result.title;
+      const explanation = result.explanation;
+      console.log(result);
+      $('#container').html(`<img src="${imgs}" alt="">`);
+    }
+  });
+}
 
 $('#btnSearch').click(function() {
   const day = $('#day').val();
@@ -25,8 +39,22 @@ $('#btnSearch').click(function() {
       const title = result.title;
       const explanation = result.explanation;
       console.log(result);
-      $('#gallery').html(`<img src="${imgs}" alt="">`);
+      $('#gallery').html(`
+        <div class="base">
+          <div class="fondo">
+            <img src="${imgs}" alt="">
+          </div>
+          <div class="title">
+            <h2>${title}</h2>
+          </div>
+        </div>`
+      );
     }
   });
 });
 
+
+// $("p").hide(1000);
+//     $("button").click(function(){
+//         $("p").show(1000);
+//     });

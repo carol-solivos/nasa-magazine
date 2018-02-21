2a1dd8ee84116f10bb1a0f79dcdc9c95921a4378
carol-solivos <carol.solivos@gmail.com>
2018-02-21 12:24:01 -0400

    arreglando slide

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,53 +8,105 @@ function toDayImage() {
   let toDay = d.getDate();
   let toMonth = d.getMonth();
   let toYear = d.getFullYear();
-
+  // let patt = ^[0-9]+$;
   if (toDay < 10) {
     toDay = '0' + toDay;
   };
-
   let url = `https://api.nasa.gov/planetary/apod?date=${toYear}-${arrMonth[toMonth]}-${toDay}&api_key=Tqbm56QokO6KA0u6wjbcnSVeiYwlsq9dNK46y7fE`;
-
   $.ajax({
     url: url,
     success: function(result){
       const imgs = result.url;
       const title = result.title;
       const explanation = result.explanation;
-      console.log(result);
-      $('#container').html(`<img src="${imgs}" alt="">`);
+      const media = result.media_type;
+      if (media === 'video') {
+        console.log(result);
+        $('#container').html(`
+          <iframe width="100%" height="600px"
+          src="${imgs}&autoplay=1">
+          </iframe>`     
+        );
+        $('#descrip').html(`
+          <div class="title">
+          <h2>${title}</h2>
+          </div>`     
+        );
+        $('#descrip').append(`<p class="exp">${explanation}</p>`);
+      }
+      if (media === 'image') {
+        console.log(result);
+        $('#container').html(`
+          <div class="fondo">
+          <img src="${imgs}" alt="">
+          </div>`
+        );
+        $('#descrip').html(`
+          <div class="title">
+          <h2>${title}</h2>
+          </div>`     
+        );
+        $('#descrip').append(`<p class="exp">${explanation}</p>`);
+      }      
     }
   });
 }
 
+//filtro para buscar imagen por fecha
 $('#btnSearch').click(function() {
   const day = $('#day').val();
   const month = $('#month').val();
   const year = $('#year').val();
+  if (day.length > 2) {
+    alert('Pon un día de dos dígitos numéricos')
+  }
   let url = `https://api.nasa.gov/planetary/apod?date=${year}-${month}-${day}&api_key=Tqbm56QokO6KA0u6wjbcnSVeiYwlsq9dNK46y7fE`;
   $.ajax({
     url: url,
-    success: function(result){
+    success: function (result){
       const imgs = result.url;
       const title = result.title;
       const explanation = result.explanation;
-      console.log(result);
-      $('#gallery').html(`
-        <div class="base">
-          <div class="fondo">
-            <img src="${imgs}" alt="">
-          </div>
+      const media = result.media_type;
+      if (media === 'video') {
+        console.log(result);
+        $('#container').html(`
+          <iframe width="100%" height="600px"
+          src="${imgs}&autoplay=1">
+          </iframe>`     
+        );
+        $('#descrip').html(`
           <div class="title">
-            <h2>${title}</h2>
-          </div>
-        </div>`
-      );
-    }
+          <h2>${title}</h2>
+          </div>`     
+        );
+      }
+      if (media === 'image') {
+        console.log(result);
+        $('#container').html(`
+          <div class="fondo">
+          <img src="${imgs}" alt="">
+          </div>`
+        );
+        $('#descrip').html(`
+          <div class="title">
+          <h2>${title}</h2>
+          </div>`     
+        );
+      }
+      $('#descrip').append(`<p class="exp">${explanation}</p>`);
+      $('#descrip2').html(`<div id="descrip2"><i class="fa fa-arrow-down"></i> Ocultar descripción</div>`);
+    } 
   });
 });
 
-
-// $("p").hide(1000);
-//     $("button").click(function(){
-//         $("p").show(1000);
-//     });
+$('#descrip2').click(function() {
+  $('#content').html(`<div id="descrip3"><i class="fa fa-arrow-up"></i> Mostrar descripción</div>`);
+  $('#descrip p').fadeOut();
+  $('#container img').css('height', '80vh');      
+})
+$('#descrip3').click(function() {
+  $('#content').html(`<div id="descrip2"><i class="fa fa-arrow-down"></i> Ocultar descripción</div>`);
+  $('#descrip p').show();
+  $('#container img').css('height', '60vh'); 
+})

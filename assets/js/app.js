c17105036eb7a754e3ff7439fc0c6c4712b55080
carol-solivos <carol.solivos@gmail.com>
2018-02-22 09:13:19 -0400

    slide sin bug

--- assets/js/app.js
+++ assets/js/app.js
@@ -95,7 +95,7 @@ $('#btnSearch').click(function() {
         );
       }
       $('#descrip').append(`<p class="exp">${explanation}</p>`);
-      $('#descrip2').html(`<div id="descrip2"><i class="fa fa-arrow-down"></i> Ocultar descripción</div>`);
+      $('#content').html(`<div id="descrip2"><i class="fa fa-arrow-down"></i> Ocultar descripción</div>`);
     } 
   });
 });
